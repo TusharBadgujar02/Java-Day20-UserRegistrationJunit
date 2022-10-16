3b68f2f729ca32d9527c2cacdc3f78ba7e765433
9685959802 <tusharbadgujar0000@gmail.com>
2022-10-16 19:44:52 +0530

    Enter Valid Email ID

--- UserRegistration.java
+++ UserRegistration.java
@@ -18,6 +18,9 @@ public class UserRegistration {
 
 		System.out.println("Enter Last Name :");
 		validateName(getUserInput());
+		
+		System.out.println("\nEnter Email Id : ");
+		validateEmail(getUserInput());
 
 	}
 
@@ -25,6 +28,21 @@ public class UserRegistration {
 		Scanner sc = new Scanner(System.in);
 		return sc.nextLine();
 	}
+	
+public void validateEmail(String input) {
+		
+		String email = "^[0-9a-zA-Z]+([_+-.a-z0-9A-Z]+)*[@][a-zA-Z]+[.][a-z]{2,4}([.][a-z]{2})?$";
+		
+		System.out.println("\nValidating Email...");
+		match(email, input);
+	}
+
+public void match(String regex, String input) {
+	Pattern pattern = Pattern.compile(regex);
+	Matcher match = pattern.matcher(input);
+	
+	printResult(match.matches(), input);
+}
 
 	public void validateName(String input) {
 
@@ -44,7 +62,4 @@ public class UserRegistration {
 		else
 			System.out.println(" " + input + " is Invalid");
 	}
-
-
-
 }
